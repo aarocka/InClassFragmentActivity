667d8fe30d53f3c557eb80c1b8add4667eff5143
Aaron <aarocka@protonmail.com>
2023-03-23 15:06:40 -0400

    did some interface ViewModel atuff

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -3,16 +3,23 @@ package edu.temple.inclassactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
+
+        mainViewModel.setImageIds(imageArray)
 
         //log the imageArray
         for (i in imageArray.indices) {
@@ -23,12 +30,20 @@ class MainActivity : AppCompatActivity() {
         // newInstance() to the FrameLayout in activity_main.xml
         supportFragmentManager
             .beginTransaction()
-            .add(R.id.fragmentContainerView, ImageDisplayFragment.newInstance(imageArray))
+            .add(R.id.fragmentContainerView, ImageDisplayFragment())
             .commit()
 
 
 
 
+        /*
+        // Create an instance of the interface
+        fun imageSelected(itemId: Int){
+            Toast.makeText(this, "Image $itemId selected", Toast.LENGTH_SHORT).show()
+        }
+        */
+
+        //
 
 
 
